a1c049c40cdc3425f6335ccba03f6871ce42621d
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2024-08-02 10:54:51 +0300

    feat(plugins): find by slug

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,66 +1,71 @@
 export default ({ env }) => ({
-    graphql: {
-        config: {
-          endpoint: '/graphql',
-          shadowCRUD: true,
-          playgroundAlways: false,
-          depthLimit: 100,
-          amountLimit: 100,
-          apolloServer: {
-            tracing: true,
-          },
+  // Ref: https://github.com/strapi-community/strapi-plugin-slugify
+  slugify: {
+    enabled: true,
+    config: {
+      contentTypes: {
+        project: {
+          field: 'slug',
+          references: 'name'
         },
-      },
-      'apollo-sandbox': {
-        enabled: env("APOLLO_SANDBOX_ENABLED", false)
-      },
-      // Ref: https://github.com/strapi-community/strapi-plugin-notes
-      'entity-notes': {
-        enabled: true,
-      },
-      "soft-delete": {
-        enabled: true,
-      },
-      //Ref: https://github.com/mancku/strapi-plugin-schemas-to-ts
-      'schemas-to-ts': {
-        enabled: true,
-      },
-      // Ref: https://github.com/strapi-community/strapi-plugin-slugify
-      slugify: {
-        enabled: true,
-        config: {
-          contentTypes: {
-            // project: {
-            //   field: 'slug',
-            //   references: 'name'
-            // }
-          }
+        person: {
+          field: 'slug',
+          references: 'slug'
         }
-      },
-      // Ref: https://github.com/strapi/strapi-plugin-seo
-      seo: {
-        enabled: true,
-      },
-      // Ref: https://github.com/lautr/strapi-plugin-duplicate-button
-      'duplicate-button': true,
-      //Ref: https://market.strapi.io/plugins/strapi-plugin-publisher
-      publisher: {
-        enabled: true,
-        config: {
-          hooks: {
-            beforePublish: async ({ strapi, uid, entity }) => {
-              console.log('beforePublish');
-            },
-            afterPublish: async ({ strapi, uid, entity }) => {
-              console.log('afterPublish');
-            },
-            beforeUnpublish: async ({ strapi, uid, entity }) => {
-              console.log('beforeUnpublish');
-            },
-            afterUnpublish: async ({ strapi, uid, entity }) => {
-              console.log('afterUnpublish');
-            },
-          },
-        },
       }
+    }
+  },
+  graphql: {
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 100,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: true,
+      },
+    },
+  },
+  'apollo-sandbox': {
+    enabled: env("APOLLO_SANDBOX_ENABLED", false)
+  },
+  // Ref: https://github.com/strapi-community/strapi-plugin-notes
+  'entity-notes': {
+    enabled: true,
+  },
+  "soft-delete": {
+    enabled: true,
+  },
+  //Ref: https://github.com/mancku/strapi-plugin-schemas-to-ts
+  'schemas-to-ts': {
+    enabled: true,
+  },
+
+  // Ref: https://github.com/strapi/strapi-plugin-seo
+  seo: {
+    enabled: true,
+  },
+  // Ref: https://github.com/lautr/strapi-plugin-duplicate-button
+  'duplicate-button': true,
+  //Ref: https://market.strapi.io/plugins/strapi-plugin-publisher
+  publisher: {
+    enabled: true,
+    config: {
+      hooks: {
+        beforePublish: async ({ strapi, uid, entity }) => {
+          console.log('beforePublish');
+        },
+        afterPublish: async ({ strapi, uid, entity }) => {
+          console.log('afterPublish');
+        },
+        beforeUnpublish: async ({ strapi, uid, entity }) => {
+          console.log('beforeUnpublish');
+        },
+        afterUnpublish: async ({ strapi, uid, entity }) => {
+          console.log('afterUnpublish');
+        },
+      },
+    },
+  }
 });
