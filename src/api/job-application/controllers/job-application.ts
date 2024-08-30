d1c53556c80e9fb5b83f744d0234d9ff81dca5f1
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2024-08-30 17:24:23 +0300

    chore(doc): swagger doc

--- src/api/job-application/controllers/job-application.ts
+++ src/api/job-application/controllers/job-application.ts
@@ -5,3 +5,17 @@
 import { factories } from '@strapi/strapi'
 
 export default factories.createCoreController('api::job-application.job-application');
+
+// export default factories.createCoreController('api::job-application.job-application', ({ strapi }) => ({
+//     async create(ctx) {
+//         const files = ctx.request.files;
+//         let application = await strapi.entityService.create('api::job-application.job-application', {
+//             data: {
+//                 ...ctx.request.body,
+//                 applicant: ctx.state.user.id
+//             },
+//             files
+//         });
+//         ctx.body = application
+//     }
+// }));
